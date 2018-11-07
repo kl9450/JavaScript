9aab23a8e44bc5135aeec9e9521be4b16b0ffe05
plamediluyeye@gmail.com <demasmarlon@gmail.com>
2018-11-07 11:53:07 +0200

    Started with JavaScript variables

---
+++ javascript/main.js
@@ -0,0 +1,65 @@
+'use strict'; //The "use strict" directive switches the engine to the "modern" mode, changing the behaviour of some built-in features
+
+
+//variables are used to store this information.
+
+//A variable is a "name storage" for data. We can use variables to store data.
+
+//To create a variable in JavaSrcipt, we need to use the let or var keyword.
+
+var message
+
+//assignment operator =
+
+//We add a string ''
+//A string in JavaScript is a set of characters surrounded by quotation marks.
+
+var message = 'Good morning';
+    // message='How are you today?';
+alert(message);
+
+// var name = 'Nate';
+//      age= 43;
+//      gender='female'
+// console.log(name)
+
+//**************************************************************************************
+//************************************************************************************** 
+
+// Always remember to close a string with the same type of quotation marks you started it with.
+
+// Operators:
+
+// + addition
+// - subtraction
+///  division
+// * multiplication
+
+// the + operator can be applied to two string values. This will join two strings together, otherwise called concaternation.
+
+// In the console "hey" + "there."
+
+//choose meaningful names
+//Don't use reserved words
+//Follow conventions
+
+/* https: //developer.mozilla.org/en-US/docs/Web/JavaScript/References/Lexical_grammer#Keywords */
+
+
+// Begin
+//          Get out the rice cooker
+//          Fill it with rice
+//          Fill it with water
+//          Cook the rice
+//          Chop the vegatables
+//          
+var name = prompt("Enter your first name:");
+alert("Hello, " + name);
+
+var name = ("Kodi")
+var age = ("21")
+var gender = ("Male")
+var place = ("Retreat")
+var gaudian = ("Mother")
+var food = ("pie")
+console.log("Hello, my name is " +name, "and I am " +age, "years old. I live in " +place, "with my " +gaudian, "and uncle. My favourite food to eat is a" +food, "because it taste very noce and comes in a variaty of flavours.")
